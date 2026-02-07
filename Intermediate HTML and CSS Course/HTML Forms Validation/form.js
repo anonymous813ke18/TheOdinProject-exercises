ead87aee6cc3e508b5cb82e50cb8321e0c28b544
anonymous813ke18 <105976740+anonymous813ke18@users.noreply.github.com>
2026-02-07 16:00:31 +0530

    Created a fully functional (client-side) contact form
    Followed along with Kevin Powell's course on Constraint
    Validation API to create this form. Added custom
    validation using JavaScript.

---
+++ Intermediate HTML and CSS Course/HTML Forms Validation/form.js
@@ -0,0 +1,42 @@
+const form = document.querySelector('form');
+
+function validateField(field) {
+    const errorElement = field.type === 'radio'
+        ? field.closest("fieldset").querySelector('.error-message')
+        : field.parentElement.querySelector('.error-message');
+
+    if (!field.validity.valid) {  
+        errorElement.textContent = field.dataset.error || "This field is required.";
+        return false;
+    }
+
+    errorElement.textContent = ""; 
+    return true;
+}
+
+form.querySelectorAll('input, textarea').forEach(input => {
+    input.addEventListener("blur", () => {
+        validateField(input);
+    })
+})
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let isValid = true;
+
+    const fields = form.querySelectorAll('input, textarea');
+
+    fields.forEach(field => {
+        console.log(`Checking ${field.name}...`);
+        const fieldValid = validateField(field);
+        if (!fieldValid) isValid = false;
+    })
+    
+    if (isValid) {
+        //send form data
+        form.reset();
+    } else {
+        form.querySelector(":invalid").focus();
+    }
+});
