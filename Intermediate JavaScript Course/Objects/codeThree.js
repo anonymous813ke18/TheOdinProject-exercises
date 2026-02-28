bcd1a020120ede2e7835e126f93222ff819c9a54
anonymous813ke18 <105976740+anonymous813ke18@users.noreply.github.com>
2026-02-28 16:15:01 +0530

    Onjects as a design patter
    We can store functions indide a object, which is the
    core concept of OOP, using a long or shorthand notation.
    We can then call that function in the following ways:
    objName.functionName() or objName["functionName"]()
    
    When using the bracket notation it is imp to put the
    functionName inside "" or it will give a Uncaught
    ReferenceError.

---
+++ Intermediate JavaScript Course/Objects/codeThree.js
@@ -0,0 +1,18 @@
+const car = {
+    make: "Volkswagen",
+    model: "Golf",
+    year: 2026,
+    color: "blue",
+    priceUSD: 40000,
+    applyDiscount: function (discountPercentage) {
+        const multiplier = 1 - discountPercentage / 100;
+        this.priceUSD *= multiplier;
+    },
+    getSummary() {
+        return `${this.year} ${this.make} ${this.model} in ${this.color}, priced at ${this.priceUSD} (USD).`;
+    },
+};
+
+console.log(car["getSummary"]());
+car["applyDiscount"](10);
+console.log(car["getSummary"]());
