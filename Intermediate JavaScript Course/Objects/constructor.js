743d25eeedb5d522bb4f1b89abea5a4eb6090123
anonymous813ke18 <105976740+anonymous813ke18@users.noreply.github.com>
2026-03-10 08:02:29 +0530

    Object Prototypes
    Removed the getName() and getMarker() functions from
    the constructor, instead added them to the Player
    prototype (Player.prototype). So now those two functions
    will be inherited by all the objects created using
    the Player constructor.
    
    Did this for effiency, if getName() and getMarker()
    were constructor functions everytime a new object
    was created using the Player constructor, those to
    functions would be duplicated, so if there are 10,000
    player there would be 20,000 duplicates of those two
    functions, but now there will only be two copies of
    those functions no matter how many objects we create
    and those copies will be shared by all objects.

--- Intermediate JavaScript Course/Objects/constructor.js
+++ Intermediate JavaScript Course/Objects/constructor.js
@@ -1,15 +1,26 @@
-function Player (name, marker) {
+function Player (name, marker, nationality) {
     this.name = name;
     this.marker = marker;
-    this.getName = function () {
-        return this.name;
-    };
-    this.getMarker = function () {
-        return this.marker;
-    };
+    this.nationality = nationality;
+
+    //
+    // this.getName = function () {
+    //     return this.name;
+    // };
+    // this.getMarker = function () {
+    //     return this.marker;
+    // };
 }
 
-const player1 = new Player("Hammad", "X");
-const player2 = new Player("also Hammad", "O");
+Player.prototype.getName = function () {
+    return this.name;
+};
 
-console.log(`Player 1 is: ${player1.getName()} with marker ${player1.getMarker()}\nPlayer 2 is: ${player2.getName()} with marker: ${player2.getMarker()}`);
+Player.prototype.getMarker = function () {
+    return this.marker;
+};
+
+const player1 = new Player("Hammad", "X", "Pakistani");
+const player2 = new Player("also Hammad", "O", "Arabian");
+
+console.log(`Player 1 is: ${player1.getName()} with marker ${player1.getMarker()} and nationality ${player1.nationality}\nPlayer 2 is: ${player2.getName()} with marker: ${player2.getMarker()} and nationality ${player2.nationality}`);
